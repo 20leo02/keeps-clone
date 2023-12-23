a27209f50eed956eaa1fcecfc1d4c0af910c8db6
20leo02 <lrm2188@columbia.edu>
2023-12-23 18:40:45 -0500

    attempt at getting apm to show logs under keeps service

--- server/server.js
+++ server/server.js
@@ -10,6 +10,21 @@ import pino from 'pino'
 
 dotenv.config();
 
+const agent = apm.start({
+    serviceName: 'keeps',
+
+    secretToken: 'Nmz6vpHwFXnOXdKO3w',
+
+    serverUrl: 'https://0ec13320672f44ac9c92b32d94cd58b7.apm.us-east-2.aws.elastic-cloud.com:443',
+
+    environment: 'keeps-env',
+
+    active: true,
+});
+
+agent.startTransaction('keeps-transaction');
+const traceparent = agent.currentTraceparent
+agent.endTransaction();
 const PORT = process.env.SERVERPORT;
 //App setup.
 const app = express();
@@ -18,19 +33,16 @@ app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json())
 
 // Add this to the very top of the first file loaded in your app
-const perf = apm.start({
-    serviceName: 'keeps',
+const log = pino(
+    ecsFormat({
+        serviceName: 'keeps',
+    }),
+    pino.destination('./app.log'));
 
-    secretToken: 'Nmz6vpHwFXnOXdKO3w',
-
-    serverUrl: 'https://0ec13320672f44ac9c92b32d94cd58b7.apm.us-east-2.aws.elastic-cloud.com:443',
-
-    environment: 'keeps-env'
-});
-
-const log = pino(ecsFormat(/* options */), pino.destination('./app.log'));
+log.info(`APM Agent status: ${apm.isStarted()?'Online':'Offline'}`)
 
 //DB connection setup.
+log.info('Connecting to database...')
 var pool = new pg.Pool({
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
@@ -48,7 +60,7 @@ log.info('Connected to database.')
 
 app.get("/api", (req, res) => {
     //Query DB to make Note information available on the API.
-    const span = perf.startSpan('db notes api data');
+    const span = agent.startSpan('db notes api data');
     const text = 'SELECT * FROM Notes';
     pool.query(text).then(result =>{
         res.send(result.rows)
@@ -65,7 +77,7 @@ app.get("/api", (req, res) => {
 app.post("/api", (req, res) => {
     const data = req.body
     if(data.type==='add'){
-        const span = perf.startSpan('api add');
+        const span = agent.startSpan('api add');
         //Insert single Note in DB
         log.info('Adding to DB')
         const text = `INSERT INTO Notes(title, content) VALUES ('${data.note.title}', '${data.note.content}')`;
@@ -74,7 +86,7 @@ app.post("/api", (req, res) => {
     }
     else if(data.type==='delete'){
         //Delete single Note from DB.
-        const span = perf.startSpan('api delete');
+        const span = agent.startSpan('api delete');
         log.info('Deleting from DB.')
         const text = `DELETE FROM Notes WHERE nid=${data.nid}`;
         pool.query(text).then(res.sendStatus(200));
